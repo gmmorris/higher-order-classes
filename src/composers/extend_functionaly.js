4889c19fc4b0d3f6ca0a7ba09a49ca6f1e2b35ea
Gidi Morris <github@gidi.io>
2015-11-01 20:22:51 +0000

    added comment

--- src/composers/extend_functionaly.js
+++ src/composers/extend_functionaly.js
@@ -1,4 +1,10 @@
-
+/*
+ * A Higher Order Function which takes a Class definition and returns a new function.
+ *
+ * @param  {Function} ClassToCompose  The class definition we wish to analyze
+ * @return {Function} A function which takes a property name and return a boolean specifying whether
+ *                    the class has a method with that name which is composable
+*/
 function isComposablePropertyOf(ClassToCompose) {
   const proto = ClassToCompose.prototype;
   return propertyName => {
